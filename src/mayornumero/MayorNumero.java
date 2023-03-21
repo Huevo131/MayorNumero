948927c8a5e108a854f09d60eea4510c346d155b
Yahir Rios <125381043+Huevo131@users.noreply.github.com>
2023-03-21 14:27:01 -0600

    Add files via upload

---
+++ src/mayornumero/MayorNumero.java
@@ -0,0 +1,37 @@
+package mayornumero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author maryse
+ * 
+ * Este programa determina el mayor de dos números ingresados y lo visualiza en 
+ * pantalla; la entrada de los datos enteros se realiza a través del uso de Scanner;
+ * por último, la selección del mayor se realiza con el * operador > y la sentencia if.
+ * 
+ */
+
+public class MayorNumero {
+    int n1, n2;
+    Scanner entrada = new Scanner(System.in);
+    
+    public void numeros(){
+         System.out.print("Introduzca primer entero: ");
+        n1 = entrada.nextInt();
+        System.out.print("Introduzca segundo entero: ");
+        n2 = entrada.nextInt();
+        if (n1 > n2) {
+            System.out.println(" El mayor es " + n1);
+        } else {
+            System.out.println(" El mayor es " + n2);
+        }
+
+    }
+    public static void main(String[] args) {
+        MayorNumero n = new MayorNumero();
+        n.numeros();
+
+    }
+
+}
